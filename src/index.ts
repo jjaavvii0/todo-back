9bdc1614d165d0e22a7b3c89f00f7b16e72d23de
J P <jjaavvii0@gmail.com>
2024-10-03 14:13:01 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import app from './app';
+
+
+const PORT: string | number = process.env.PORT || 3000;
+
+app.listen(PORT, (err?: Error) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is listening on port ${PORT}`);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.json({ status: 'OK' });
+});
